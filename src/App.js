830973dd7a1d86bbee24ea7792a22a5deffd91e8
ayush seth <ayuush.seth227@gmail.com>
2022-11-12 22:51:59 +0530

    Dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import LandingPage from "./components/LandingPage";
+import Dashboard from "./components/Dashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="App">
-			<LandingPage />
+        	<BrowserRouter>
+             <Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/dashboard" element={<Dashboard/>}/>
+             </Routes>
+           </BrowserRouter>
+			
 		</div>
 	);
 }
