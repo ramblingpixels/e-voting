e29fd980522a08ec55f49b6d012139a8f5717305
ayush seth <ayuush.seth227@gmail.com>
2022-11-14 01:58:38 +0530

    sbt verified

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,24 +2,41 @@ import React, { useState } from "react";
 import Svimg from "../Misc/Svimg";
 import Web3 from "web3";
 
-let tokenAbi = [
-{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-];
+
 const web3 = new Web3('https://bsc-dataseed1.binance.org:443');
 const LandingPage = () => {
 	const [walletAddress, setWalletAddress] = useState("");
-
 	async function requestAccount() {
 		console.log("Connect Wallet");
 
 		if (window.ethereum) {
+
 			try {
-				console.log("Connected");
+			    console.log("Connected");
 				const account = web3.eth.accounts;
 				setWalletAddress(account.givenProvider.selectedAddress);
-				console.log(`Wallet: ${walletAddress}`);
-				const contract = new web3.eth.Contract(tokenAbi,"0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c");
+		     	console.log(`Wallet: ${walletAddress}`);
+
+				const minABI = [{
+                  constant: true,
+                  inputs: [{name: "_owner", type: "address"}],
+                  name: "balanceOf",
+                  outputs: [{name: "balance", type: "uint256"}],
+                  type: "function",
+                 },
+			    ];
+				
+				const tokenAddress = "0x2B09d47D550061f995A3b5C6F0Fd58005215D7c8";
+				const contract = new web3.eth.Contract(minABI, tokenAddress);
+				contract.defautAccount = walletAddress;
 				console.log({ contract });
+				const result = await contract.methods.balanceOf(walletAddress).call()
+				console.log(result);
+
+				// const contract = new web3.eth.Contract(tokenAbi, "0x2B09d47D550061f995A3b5C6F0Fd58005215D7c8");
+				// contract.defautAccount = walletAddress;
+				// console.log({ contract });
+				// console.log(tokenAbi);
 			
 			} catch (error) {
 				console.log("Error");
